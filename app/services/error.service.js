3a980b72750cab568bca418b60d3730e8fca0fc0
Evan Duncan <eduncan@tapqa.com>
2016-01-09 09:58:05 -0600

    feat(service) Add error service to format response objects

---
+++ app/services/error.service.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var _ = require('lodash');
+
+/**
+ * Create an error response object
+ * @param {Array|String} errors - An array of errors or one error string
+ * @returns {Array[Object]}
+ */
+exports.createError = function (errors) {
+    var errObject = { errors: [ ] };
+
+    if (errors instanceof Array) {
+        _.forEach(errors, function (err) {
+            errObject.errors.push({ message: err });
+        });
+    } else {
+        errObject.errors.push({ message: errors });
+    }
+
+    return errObject;
+};
---
+++ app/tests/services/error.service.test.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var errorService = require('../../services/error.service'),
+    results;
+
+/**
+ * Helper to generate random string
+ * @returns {String}
+ */
+function getRandomStr() {
+    return Math.random().toString(36).substring(7);
+}
+
+describe('Error Service', function () {
+    describe('initialization', function () {
+        it('should have a createError function', function () {
+            errorService.should.have.property('createError')
+                .and.is.instanceOf(Function);
+        });
+    });
+
+    describe('#createError()', function () {
+        beforeEach(function () {
+            results = errorService.createError('This is an error');
+        });
+
+        it('should return a collection of errors', function () {
+            results.should.be.instanceOf(Object).and.have.property('errors');
+            for (var i = 0; i < results.errors.length; i++) {
+                var error = results.errors[i];
+                error.should.be.instanceOf(Object).and.have.property('message');
+            }
+        });
+
+        it('should return one error object if a string param was given', function () {
+            results.errors.length.should.equal(1);
+        });
+
+        it('should return as many error objects as there were strings within array param given', function () {
+            var errCnt = Math.floor(Math.random() * 100);
+            var arrayParam = [];
+
+            for (var i = 0; i < errCnt; i++) {
+                arrayParam.push(getRandomStr());
+            }
+
+            results = errorService.createError(arrayParam);
+            results.errors.length.should.equal(errCnt);
+        });
+    });
+});
