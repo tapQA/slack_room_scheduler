8c01e2da0f6e5a30b536904540af0722cbc52698
Evan Duncan <eduncan@tapqa.com>
2016-01-09 21:37:21 -0600

    feat(express) Configure express

--- config/env/all.js
+++ config/env/all.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = {
+    port: process.env.PORT || '3000',
     outlookApiURI: 'https://outlook.office.com/api/v2.0',
     outlookApiKey: process.env.OUTLOOK_API_KEY || '',
     slackApiURI: 'https://slack.com/api',
--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 'use strict';
 
-var express = require('express'),
-    app = express(),
-    config = require(__dirname + '/config/config');
+var config = require(__dirname + '/config/config');
 
+var app = require(__dirname + '/config/express')(/* optional db connection */);
 
-app.listen(8000, function () {
-    console.log('API listening on port 8000');
-});
+app.listen(config.port);
+console.log('API started on port ' + config.port);
